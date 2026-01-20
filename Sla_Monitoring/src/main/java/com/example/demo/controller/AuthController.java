c321387679cd415ecf6150ff915d847dc17978fc
Kalaivanikrupa <kalaivanikrupanandam30@gmail.com>
2026-01-20 19:26:42 +0330

    change of cross origin

--- Sla_Monitoring/src/main/java/com/example/demo/controller/AuthController.java
+++ Sla_Monitoring/src/main/java/com/example/demo/controller/AuthController.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000") // Frontend connect panna
+@CrossOrigin(origins = "*") // Frontend connect panna
 public class AuthController {
 
     @Autowired
